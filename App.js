514142e22245328e3b9f7b9cf34ee6dce6a98d4f
simonojok19 <simonojok19@gmail.com>
2021-06-08 00:33:52 +0300

    learning basic elements

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text>Hello, World!</Text>
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'teal',
+    paddingVertical: 10,
+    paddingHorizontal: 30
+  }
 });
+
+export default App
