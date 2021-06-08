42c72ee84acb4c62bfe101ef2e7f98719670d848
simonojok19 <simonojok19@gmail.com>
2021-06-08 09:23:23 +0300

    added the cyan box styles

--- App.js
+++ App.js
@@ -4,22 +4,35 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 const App = () => {
   return (
       <SafeAreaView style={styles.safeArea}>
-        <View style={[styles.container]}>
-          <Text>Hello, World!</Text>
+        <View style={styles.container}>
+          <Text style={[styles.text]}>Here are some boxes of different colors</Text>
+          <View style={[styles.cyanBox]}>
+              <Text style={[styles.boxText]}>Cyan: #2aa198</Text>
+          </View>
         </View>
       </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
+  text: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 40
   },
-  safeArea: {
-    flex: 1
+  container: {
+      paddingHorizontal: 10,
+      paddingTop: 40
+  },
+  cyanBox: {
+      backgroundColor: "#2aa198",
+      padding: 10,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  boxText: {
+      color: "white",
+      fontWeight: "bold"
   }
 });
 
