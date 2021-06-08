9fe374e2dc5237d6c430a1785eb862a2532c0ff2
simonojok19 <simonojok19@gmail.com>
2021-06-08 21:19:42 +0300

    removed the duplicated codes

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import {View, Text, SafeAreaView, StyleSheet, FlatList} from "react-native";
-import ColorBox from "./components/ColorBox";
 import {NavigationContainer} from "@react-navigation/native";
 
 const App = () => {
   return (
       <NavigationContainer>
-          <SafeAreaView style={styles.safeArea}>
-              <FlatList data={COLORS} renderItem={
-                  ({item, index}) => <ColorBox colorName={item.colorName} hexCode={item.hexCode}/>}
-                        keyExtractor={item => item.colorName}
-                        ListHeaderComponent={<Text>Solarized Color</Text>}
-              />
-          </SafeAreaView>
+
       </NavigationContainer>
   );
 }
