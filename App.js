fbed1dbe3ac9b4c67ad209da7acf26ebe5e5bfc3
simonojok19 <simonojok19@gmail.com>
2021-06-08 21:22:42 +0300

    creating stack navigations

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import ColorPalette from "./screens/ColorPalette";
+import Home from "./screens/Home";
 
+const Stack = createStackNavigator()
 const App = () => {
   return (
       <NavigationContainer>
-
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="ColorPalette" component={ColorPalette}/>
+          </Stack.Navigator>
       </NavigationContainer>
   );
 }
