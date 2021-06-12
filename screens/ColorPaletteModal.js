27ef75c1b55b513c1337cde8fe4ce53e1c379493
simonojok19 <simonojok19@gmail.com>
2021-06-12 12:26:29 +0300

    added the button

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, TouchableOpacity, StyleSheet, TextInput} from 'react-native'
 
 const ColorPaletteModal = () => {
+    const {name, setName} = useState('');
     return (
-        <View>
-            <Text>Hello, from modal</Text>
+        <View style={styles.container}>
+            <TextInput style={styles.input} value={name} onChangeText={setName}/>
+            <TouchableOpacity>
+                <Text>Submit</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 5
+    },
+    container: {
+        padding: 5,
+        backgroundColor: 'white',
+        flex: 1
+    }
+})
 
 export default ColorPaletteModal
