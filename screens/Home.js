55f45a8ab854b44ad7221b439a05bb4d7b320147
simonojok19 <simonojok19@gmail.com>
2021-06-12 07:22:02 +0300

    added the refrshing part of the flat list

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,24 @@
 import React, {useState, useEffect, useCallback} from "react";
-import {FlatList, StyleSheet} from "react-native";
+import {FlatList, StyleSheet, RefreshControl} from "react-native";
 import PalettePreview from "../components/PalettePreview";
 
 const Home = ({navigation}) => {
     const [colors, setColors] = useState([]);
+    const [isRefreshing, setRefreshing] = useState(true);
+
     const fetchColors = useCallback(() => {
         fetch("https://color-palette-api.kadikraman.now.sh/palettes")
             .then(response => response.json())
             .then(data => setColors(data))
+            .then(() => {
+                setRefreshing(false)
+            })
             .catch(error => console.log(error, "error"))
+    }, []);
+
+    const handleRefresh = useCallback(() => {
+        setRefreshing(true)
+        fetchColors();
     }, []);
 
     useEffect(() => {
@@ -16,6 +26,8 @@ const Home = ({navigation}) => {
     }, []);
     return (
         <FlatList
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
             style={styles.list}
             data={colors} keyExtractor={item => item.paletteName} renderItem={({item, index}) => (
             <PalettePreview
