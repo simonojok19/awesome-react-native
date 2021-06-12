ce2d29c2df88c4da8a6a90728385fc61c4d5ec08
simonojok19 <simonojok19@gmail.com>
2021-06-12 06:13:28 +0300

    displaying colors now

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,10 +1,16 @@
 import React from "react";
-import {TouchableOpacity, Text, StyleSheet} from "react-native";
+import {TouchableOpacity, Text, StyleSheet, FlatList} from "react-native";
 
 const PalettePreview = ({handlePress, colorPalette}) => {
     return (
         <TouchableOpacity onPress={handlePress}>
             <Text style={styles.text}>{colorPalette.paletteName}</Text>
+            <FlatList
+                horizontal={true}
+                data={colorPalette.colors.slice(0, 5)}
+                keyExtractor={item => item.colorName}
+                renderItem={({ item}) => <Text>{item.colorName}</Text>}
+                />
         </TouchableOpacity>
     )
 }
