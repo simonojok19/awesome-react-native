3ef85ad8445dc05d646c782a487a313baee32f64
simonojok19 <simonojok19@gmail.com>
2021-06-12 12:45:28 +0300

    fixed some bugs

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react'
-import {View, Text, TouchableOpacity, StyleSheet, TextInput} from 'react-native'
+import React, {useState, useCallback} from 'react'
+import {View, Text, TouchableOpacity, StyleSheet, TextInput, Alert} from 'react-native'
 
-const ColorPaletteModal = () => {
-    const {name, setName} = useState('');
+const ColorPaletteModal = ({navigation}) => {
+    const [name, setName] = useState('');
+
+    const handleSubmit = useCallback(() => {
+        if (!name){
+            Alert.alert("Please enter a palette name")
+        } else {
+            navigation.navigate('Home')
+        }
+    }, [name]);
     return (
         <View style={styles.container}>
-            <TextInput style={styles.input} value={name} onChangeText={setName}/>
-            <TouchableOpacity>
-                <Text>Submit</Text>
+            <Text style={styles.name}>Name of the palette</Text>
+            <TextInput style={styles.input} value={name} onChangeText={setName} placeholder="Palette name"/>
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Submit</Text>
             </TouchableOpacity>
         </View>
     );
@@ -18,12 +27,27 @@ const styles = StyleSheet.create({
         borderColor: 'grey',
         borderWidth: 1,
         padding: 5,
-        borderRadius: 5
+        borderRadius: 5,
+        marginBottom: 40
     },
     container: {
-        padding: 5,
+        padding: 10,
         backgroundColor: 'white',
         flex: 1
+    },
+    button: {
+        height: 40,
+        backgroundColor: 'teal',
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    name: {
+        marginBottom: 10
     }
 })
 
